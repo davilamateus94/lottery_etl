b680e0a79959de0960e248944f034b957b2efd03
davilamateus94 <davilamateus94@gmail.com>
2021-08-01 17:32:07 -0300

    Creating counting sequences funcions

--- src/functions.py
+++ src/functions.py
@@ -30,7 +30,8 @@ def create_games():
     return con
 
 def get_lottery_results():
-    """Get the numbers from contests won.
+    """Get the numbers from won contests .
+
     Returns:
        String: path for the csv_file with the numbers of the winning contests
     """
@@ -53,4 +54,86 @@ def get_lottery_results():
     df.to_csv(path+'result_games.csv',index = False) 
         
     return path+'result_games.csv'
+ 
+
+def count_odds_test(*args):
+    """Count the odds numbers in a list of integers.
+
+    Returns:
+        Integer: Count of the odds numbers in a numbers list.
+    """
+    list_numbers = [arg for arg in args]
+    odd_count = len(list(filter(lambda x: (x%2 != 0) , list_numbers)))
+    return odd_count 
+
+def count_sequence_6(*args):
+    """Checks if determinated list have in maximum 6 numbers in sequence.
+       For example:
+       list1 = [1,2,3,4,5,6,10,11,12,17]
+       list2 = [1,2,3,4,5,6,7,8,9,10]
+        
+       The list1 will return True and the list2 will return False.  
+   
+    Returns:
+        Boolean: True if the list have in maximum 6 numbers in sequense, otherwise False .
+    """
+
+    retlist = []
+    count = 1
+    random_list = [arg for arg in args]
     
+    # Avoid IndexError for  random_list[i+1]
+    for i in range(len(random_list) - 1):
+        # Check if the next number is consecutive
+        if random_list[i] + 1 == random_list[i+1]:
+            count += 1
+        else:
+            # If it is not append the count and restart counting
+            retlist.append(count)
+            count = 1
+    # Since we stopped the loop one early append the last count
+    retlist.append(count)
+    
+    filtered_numbers = [number for number in retlist if number > 6]
+    
+    if len(filtered_numbers) == 0:
+        return True
+    else:
+        return False
+
+def count_sequence_4(*args):
+    """Checks if determinated list have in maximum 4 missing sequential numbers.
+       For example:
+       list1 = [1,2,3,4,5,6,10,11,12,17]
+       list2 = [1,2,3,4,5,6,11,12,13,14]
+
+        The list1 will return True and the list2 will return False. 
+
+    Returns:
+        Boolean: True if the list have in maximum 4 missing numbers in a sequential, otherwise False .
+    """
+    
+    index_list = []
+    random_list = [arg for arg in args]
+    diferences_list = []
+       # Avoid IndexError for  random_list[i+1]
+    for i in range(len(random_list) - 1):
+        # Check if the next number is consecutive
+        if random_list[i] + 1 == random_list[i+1]:
+            continue
+        else:
+            # If it is not append the index where the sequence was broken
+            index_list.append(i)
+            
+    for i in range(len(index_list)):
+       
+        position = index_list[i]
+        result = random_list[position +1] - random_list[position]
+        diferences_list.append(abs(result))      
+
+    filtered_numbers = [number for number in diferences_list if number > 4]
+    
+    if len(filtered_numbers) == 0:
+        return True
+    else:
+        return False
